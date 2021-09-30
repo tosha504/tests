5c2ce367c36f65c4a2689f65c3b59a0c4114c76b
tosha504 <tosha.rovniy@gmail.com>
2021-10-01 00:56:15 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+  $(".partners__slider").slick({
+    arrows: false,
+    infinite: true,
+    dots: false,
+    cssEase: "linear",
+    slidesToShow: 3,
+  });
+});
