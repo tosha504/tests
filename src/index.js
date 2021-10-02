b22f7961954a1692c2f3316f8a2cf1ed1807f887
tosha504 <tosha.rovniy@gmail.com>
2021-10-02 23:01:19 +0200

    add burger

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 jQuery(document).ready(function($) {
+  const $burger = $(".header__burger"),
+    $nav = $(".navigation"),
+    $body = $("body");
+
+  $burger.on("click", function(event) {
+    $burger.toggleClass("active");
+    $nav.toggleClass("active");
+    $body.toggleClass("fixed-page");
+  });
+
   $(".partners__slider").slick({
     arrows: false,
     infinite: true,
